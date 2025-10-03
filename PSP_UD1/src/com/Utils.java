aefcd360befffd68aee02f7e5cf081b30a5d06ef
Pablo <jdommay863@g.educaand.es>
2025-10-03 20:50:27 +0200

    Agregado la clase utils

---
+++ PSP_UD1/src/com/Utils.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+	
+	public static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
+	
+
+	  public static List<String> sh(String cmd) {
+	    return isWindows() ? Arrays.asList("cmd","/c",cmd)
+	                       : Arrays.asList("sh","-c",cmd);
+	  }
+	  
+	/*  public static void main(String [] args) {
+		  System.out.println(System.getProperty("os.name"));
+	  }*/
+
+}
